78f00267f97881dbd665d4fa65c3e49ba4e63445
Annette Israel <aisrael@follett.com>
2019-02-25 11:51:14 -0600

    playing with micrometer

--- src/main/java/io/vertx/launcher/MyLauncher.java
+++ src/main/java/io/vertx/launcher/MyLauncher.java
@@ -1,10 +1,16 @@
 package io.vertx.launcher;
 
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import io.vertx.core.Launcher;
+import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.micrometer.MicrometerMetricsOptions;
 import io.vertx.micrometer.VertxJmxMetricsOptions;
+import io.vertx.micrometer.VertxPrometheusOptions;
+import io.vertx.micrometer.backends.BackendRegistries;
 
 public class MyLauncher extends Launcher {
 
@@ -17,16 +23,40 @@ public class MyLauncher extends Launcher {
 
   @Override
   public void beforeStartingVertx(VertxOptions options) {
+
+//    Metrics.addRegistry(new SimpleMeterRegistry());
+//    CompositeMeterRegistry registry = (CompositeMeterRegistry) BackendRegistries.getDefaultNow();
+
+
+
     // Customize the options
     options.setWorkerPoolSize(20);
 
     options.setMetricsOptions(
       new MicrometerMetricsOptions()
-        .setJmxMetricsOptions(new VertxJmxMetricsOptions()
-          .setDomain("localhost")
-          .setStep(5)
-          .setEnabled(true))
+        .setPrometheusOptions(new VertxPrometheusOptions().setEnabled(true))
+//        .setJmxMetricsOptions(new Pro
+//          .setEnabled(true))
         .setEnabled(true));
+  }
+
+  @Override
+  public void afterStartingVertx(Vertx vertx) {
+
+//        JmxMeterRegistry registry = (JmxMeterRegistry) BackendRegistries.getDefaultNow();
+//    Metrics.addRegistry(new SimpleMeterRegistry());
+//    CompositeMeterRegistry registry = (CompositeMeterRegistry) BackendRegistries.getDefaultNow();
+
+//    registry.config().meterFilter(new MeterFilter() {
+//      @Override
+//      public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
+//        return DistributionStatisticConfig.builder()
+//          .percentiles(0.50, 0.90, 0.95, 0.99)
+//          .percentilesHistogram(true)
+//          .build()
+//          .merge(config);
+//      }
+//    });
   }
 
   @Override
